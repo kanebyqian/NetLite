f68384a8a0568b6e5ca8afe7cd0ef7c07b441eab
digiisle <kane.qyk@outlook.com>
2026-08-09 17:41:59 +0800

    rebuild

--- Images/exchange_img.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-
-logo = Image.open('D:/FunctionalTool/MyScripts/NetToolbox/Images/Network.png') 
-logo.save('D:/FunctionalTool/MyScripts/NetToolbox/Images/gNetwork.ico',format='ICO') 
